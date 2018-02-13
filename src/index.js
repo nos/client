ce267c999ba84724b25afd28c32535ae43efd72d
Matt Huggins <matt.huggins@gmail.com>
2018-02-12 21:57:15 -0700

    Created client app skeleton

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+
+function render(Component) {
+  ReactDOM.render(<Component />, document.getElementById('root'));
+}
+
+render(App);
+registerServiceWorker();
+
+if (module.hot) {
+  module.hot.accept('./App', () => {
+    const NextApp = require('./App').default;
+    render(NextApp);
+  })
+}
