ad498b0d30fe3ccf318c3d7bd3c9c9179552b8bb
Matt Huggins <matt.huggins@gmail.com>
2018-02-14 21:35:37 -0700

    Added redux store

--- src/components/Root/Root.js
+++ src/components/Root/Root.js
@@ -1,15 +1,22 @@
 import React from 'react';
 import { HashRouter } from 'react-router-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 
 import App from '../App';
 import Routes from '../Routes';
+import reducers from '../../reducers';
+
+const store = createStore(reducers);
 
 export default function Root() {
   return (
     <HashRouter>
-      <App>
-        <Routes />
-      </App>
+      <Provider store={store}>
+        <App>
+          <Routes />
+        </App>
+      </Provider>
     </HashRouter>
   );
 }
