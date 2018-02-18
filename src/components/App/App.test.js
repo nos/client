08816793fc97c453c474f2192b2af77da7135db2
Matt Huggins <matt.huggins@gmail.com>
2018-02-18 17:06:42 -0600

    Wrapped app tests with descriptive context

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -4,8 +4,10 @@ import { MemoryRouter } from 'react-router-dom';
 
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<MemoryRouter><App /></MemoryRouter>, div);
-  ReactDOM.unmountComponentAtNode(div);
+describe('<App />', () => {
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<MemoryRouter><App /></MemoryRouter>, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
 });
