7bb7813636012093c21de512be14fe641b49108c
Matt Huggins <matt.huggins@gmail.com>
2018-03-20 22:04:54 -0500

    Removed app header

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,18 +2,13 @@ import React from 'react';
 import { bool, node } from 'prop-types';
 
 import Navigation from './Navigation';
-import logo from './logo.svg';
 import styles from './App.scss';
 
 export default function App(props) {
   return (
     <div className={styles.app}>
-      <header className={styles.header}>
-        <img src={logo} className={styles.logo} alt="logo" />
-        <h1 className={styles.title}>Welcome to React</h1>
-      </header>
       <Navigation authenticated={props.authenticated} />
-      <div>
+      <div className={styles.content}>
         {props.children}
       </div>
     </div>
