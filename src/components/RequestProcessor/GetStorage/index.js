0ef5588ab114aff710abb1f7411b0c5e1b95e8ee
Jeroen Peeters <jeroen.peeters@evance.be>
2018-04-04 20:46:06 +0200

    chore(dapp): fix nullloader

--- src/components/RequestProcessor/GetStorage/index.js
+++ src/components/RequestProcessor/GetStorage/index.js
@@ -1,8 +1,9 @@
-import { withCall, withNullLoader } from 'spunky';
+import { withCall } from 'spunky';
 import { compose } from 'recompose';
 
 import GetStorage from './GetStorage';
 import storageActions from '../../../actions/storageActions';
+import withNullLoader from '../../../hocs/dapps/withNullLoader';
 
 export default compose(
   withCall(storageActions, (params) => ({ net: 'TestNet', ...params })),
