473935a4f31fcf9f4316d15db6fe3fe4e8ecea68
Matt Huggins <matt.huggins@gmail.com>
2018-04-03 22:46:44 -0500

    Updated to get requests state safely

--- src/components/RequestProcessor/index.js
+++ src/components/RequestProcessor/index.js
@@ -1,9 +1,10 @@
 import { connect } from 'react-redux';
+import { get } from 'lodash';
 
 import RequestProcessor from './RequestProcessor';
 
 const mapStateToProps = (state) => ({
-  request: state.requests[0]
+  request: get(state, 'requests.0')
 });
 
 export default connect(mapStateToProps)(RequestProcessor);
