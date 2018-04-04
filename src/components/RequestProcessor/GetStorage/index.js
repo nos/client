5a4a05457b36b915114bf3aacc34df71d00421b5
Jeroen Peeters <jeroen.peeters@evance.be>
2018-04-04 20:48:53 +0200

    chore(dapp): refactoring

--- src/components/RequestProcessor/GetStorage/index.js
+++ src/components/RequestProcessor/GetStorage/index.js
@@ -1,11 +1,14 @@
-import { withCall } from 'spunky';
+import { withCall, withData } from 'spunky';
 import { compose } from 'recompose';
 
 import GetStorage from './GetStorage';
 import storageActions from '../../../actions/storageActions';
 import withNullLoader from '../../../hocs/dapps/withNullLoader';
 
+const mapStorageDataToProps = (response) => ({ response });
+
 export default compose(
   withCall(storageActions, (params) => ({ net: 'TestNet', ...params })),
-  withNullLoader(storageActions)
+  withNullLoader(storageActions),
+  withData(storageActions, mapStorageDataToProps)
 )(GetStorage);
