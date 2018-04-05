4ebfc9f6b6a1b25507a5b348c5e3a5f5e1ac503f
DalderupMaurice <Maurice_Dalderup@hotmail.com>
2018-04-05 09:47:54 +0200

    chore(invokeActions): updated method

--- src/actions/invokeActions.js
+++ src/actions/invokeActions.js
@@ -1,29 +1,37 @@
-import Neon, {api} from '@cityofzion/neon-js';
-import {createActions} from 'spunky';
+import Neon, { api, u } from '@cityofzion/neon-js';
+import { createActions } from 'spunky';
 
 export const ID = 'testinvoke';
+const s2h = u.str2hexstring;
 
-export const testInvoke = async (net, {scriptHash, operation, args}) => {
+export const testInvoke = async (net, { scriptHash, operation, args }) => {
 
-  console.log(net);
-  console.log(scriptHash);
-  console.log(operation);
-  console.log(args);
+  const endpoint = await api.loadBalance(api.getRPCEndpointFrom, { net });
 
-  const endpoint = await api.loadBalance(api.getRPCEndpointFrom, {net});
+  const myArg = s2h(args[0]);
 
-  // Create script
-  const script = Neon.create.script(scriptHash); // TODO scriptHash --> invoke
-
-  const response = await Neon.rpc.Query.invokeScript(script).execute(endpoint);
-  console.log(response.result);
-
-  return {
-    response: 'test'
+  const invoke = {
+    scriptHash,
+    operation,
+    args: myArg
   };
+
+  try {
+    // Create script
+    const script = Neon.create.script(invoke);
+
+    const response = await Neon.rpc.Query.invokeScript(script).execute(endpoint);
+    console.log(response.result);
+
+    return {
+      response: 'test'
+    };
+  } catch(e) {
+    console.log('error', e);
+  }
 };
 
 
-export default createActions(ID, ({net, scriptHash, operation, args}) => async () => {
-  return testInvoke(net, {scriptHash, operation, args});
+export default createActions(ID, ({ net, scriptHash, operation, args }) => async () => {
+  return testInvoke(net, { scriptHash, operation, args });
 });
