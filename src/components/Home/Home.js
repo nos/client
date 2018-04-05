1036e97ab1db4b4397986924e95d4aaebf849b4d
Matt Huggins <matt.huggins@gmail.com>
2018-04-05 09:10:48 -0500

    Added wallet panel to home screen

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,12 +1,19 @@
 import React from 'react';
+import { string, objectOf } from 'prop-types';
 
+import WalletPanel from './WalletPanel';
+import balanceShape from '../../shapes/balanceShape';
 import styles from './Home.scss';
 
-export default function Home() {
+export default function Home(props) {
   return (
     <div className={styles.home}>
-      <h1>Home</h1>
-      <p>Coming soon...</p>
+      <WalletPanel address={props.address} balances={props.balances} />
     </div>
   );
 }
+
+Home.propTypes = {
+  address: string.isRequired,
+  balances: objectOf(balanceShape).isRequired
+};
--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,1 +1,22 @@
-export { default } from './Home';
+import { compose } from 'recompose';
+import { withCall, withData, withProgressComponents, progressValues } from 'spunky';
+
+import Home from './Home';
+import Loading from '../Loading';
+import authActions from '../../actions/authActions';
+import balancesActions from '../../actions/balancesActions';
+
+const { LOADING } = progressValues;
+
+const mapAuthDataToProps = ({ address }) => ({ address });
+
+const mapBalancesDataToProps = (balances) => ({ balances });
+
+export default compose(
+  withData(authActions, mapAuthDataToProps),
+  withCall(balancesActions, ({ address }) => ({ address, net: 'TestNet' })),
+  withProgressComponents(balancesActions, {
+    [LOADING]: Loading
+  }),
+  withData(balancesActions, mapBalancesDataToProps)
+)(Home);
