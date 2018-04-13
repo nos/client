37ecccb8141aa18fd8fbcc131f451caf1015e63f
Matt Huggins <matt.huggins@gmail.com>
2018-04-13 09:22:28 -0500

    Remove initial store data

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -9,21 +9,7 @@ import { identity } from 'lodash';
 import reducers from '../reducers';
 
 export default function configureStore(history) {
-  const initialState = {
-    spunky: {
-      auth: {
-        batch: false,
-        progress: 'LOADED',
-        rollbackProgress: 'LOADED',
-        loadedCount: 1,
-        data: {
-          wif: 'KxDgvEKzgSBPPfuVfw67oPQBSjidEiqTHURKSDL1R7yGaGYAeYnr',
-          address: 'AK2nJJpJr6o664CWJKi1QRXjqeic2zRp8y'
-        },
-        error: null
-      }
-    }
-  };
+  const initialState = {};
   const sagaMiddleware = createSagaMiddleware();
 
   const middleware = [thunk, sagaMiddleware, routerMiddleware(history)];
