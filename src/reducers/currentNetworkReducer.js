e64cc92e461d6ede71b8581375e1b52737dbc2d7
JackLian <hljw4@Hotmail.com>
2018-04-24 19:57:42 +1000

    Update default state in network reducer

--- src/reducers/currentNetworkReducer.js
+++ src/reducers/currentNetworkReducer.js
@@ -4,9 +4,9 @@ const initialState = {
   currentNetwork: {name: 'MainNet', neoscan: 'MainNet'},
   networks:[
     {name: 'MainNet', neoscan: 'MainNet'},
-    {name: 'default name', neoscan: 'default neoscan'},
-    {name: 'default name2', neoscan: 'default neoscan2'},
-    {name: 'default name3', neoscan: 'default neoscan3'},
+    {name: 'TestNet', neoscan: 'TestNet'},
+    {name: 'CozNet', neoscan: 'CozNet'},
+    {name: 'nOS Local', neoscan: 'nOSLocal'},
   ]
 };
 
