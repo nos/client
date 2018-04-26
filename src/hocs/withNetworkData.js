1d0b051ad2cdb6dca218a441eb31d37f93180d9e
JackLian <hljw4@Hotmail.com>
2018-04-26 23:31:01 +1000

    Fix lint errors

--- src/hocs/withNetworkData.js
+++ src/hocs/withNetworkData.js
@@ -3,11 +3,13 @@ import { withData } from 'spunky';
 import currentNetworkActions, { getAllNetworks } from '../actions/settings/currentNetworkActions';
 
 export default function withNetworkData(propName = 'net') {
-  const mapSettingsDataToProps = (currentNetwork) => ({ name: currentNetwork.name, [propName]: currentNetwork.neoscan });
+  const mapSettingsDataToProps = (currentNetwork) => ({
+    name: currentNetwork.name, [propName]: currentNetwork.neoscan
+  });
   return withData(currentNetworkActions, mapSettingsDataToProps);
 }
 
-export function withAllNetworkData(propName = 'net') {
+export function withAllNetworkData() {
   const mapSettingsDataToProps = (allNetworks) => ({ allNetworks });
   return withData(getAllNetworks, mapSettingsDataToProps);
 }
