e3c7a676205c6bfad6405488e609ec52a586f735
JackLian <hljw4@Hotmail.com>
2018-05-04 21:30:24 +1000

    Revert "Shift initialisation of nosLocal back to its own file and simplify the update network code"
    This reverts commit 608bfa20f7340a7823cc5f7949758abed60bd39f.

--- src/util/updateNetworks.js
+++ src/util/updateNetworks.js
@@ -1,9 +1,26 @@
 import { rpc, settings } from '@cityofzion/neon-js';
 import { keys } from 'lodash';
 
+import { NOS_LOCAL, PREDEFINED_NETWORKS } from '../values/networks';
+
+const { networks } = settings;
+
+const nosLocalConfig = {
+  name: NOS_LOCAL,
+  extra: {
+    neoscan: 'http://localhost:4000/api/main_net'
+  }
+};
+
 export default function updateNetworks(userNetworks) {
-  // addNetwork will overwrite existing network
-  // if they have the same name
+  keys(networks).forEach((name) => {
+    if (!PREDEFINED_NETWORKS.includes(name)) {
+      settings.removeNetwork(name);
+    }
+  });
+
+  settings.addNetwork(new rpc.Network(nosLocalConfig));
+
   userNetworks.forEach((network) => {
     settings.addNetwork(new rpc.Network(network));
   });
