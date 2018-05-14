52a7ef4ab5308e8206270aca3346fe2ff8ed8244
Matko Raguz <m.raguz@gmail.com>
2018-05-14 12:01:11 +0200

    Changed bodymoving dependency to now being pulled from public folder instead of CDN. Changed naming convention on a html element.

--- public/animations/loader.js
+++ public/animations/loader.js
@@ -2,9 +2,9 @@
  * The command is tied to script-imported library in DOM,
  * and therefore cannot have a bodymovin variable defined
  */
-// eslint-disable-next-line
+
 bodymovin.loadAnimation({
-  container: document.getElementById('bm'),
+  container: document.getElementById('animation'),
   renderer: 'svg',
   loop: true,
   autoplay: true,
