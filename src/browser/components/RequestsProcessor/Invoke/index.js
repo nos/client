36bb9ab9548272e9c1aebe543d3fca80b0daded4
Jeroen Peeters <jeroen.peeters@evance.be>
2018-05-16 10:00:37 +0200

    feat(app): use config object for invoke

--- src/browser/components/RequestsProcessor/Invoke/index.js
+++ src/browser/components/RequestsProcessor/Invoke/index.js
@@ -1,5 +1,6 @@
 import { withCall, withData } from 'spunky';
 import { compose, withProps } from 'recompose';
+import { pick } from 'lodash';
 
 import authActions from 'login/actions/authActions';
 import withNetworkData from 'shared/hocs/withNetworkData';
@@ -13,17 +14,15 @@ import withRejectMessage from '../../../hocs/withRejectMessage';
 const mapAuthDataToProps = ({ address, wif }) => ({ address, wif });
 const mapInvokeDataToProps = (txid) => ({ txid });
 
+const CONFIG_KEYS = ['scriptHash', 'operation', 'args'];
+
 export default function makeInvokeComponent(invokeActions) {
   return compose(
     // Clean redux store when done
     withClean(invokeActions),
 
     // Rename arguments given by the user
-    withProps(({ args }) => ({
-      scriptHash: args[0],
-      operation: args[1],
-      args: args.slice(2)
-    })),
+    withProps(({ args }) => pick(args[0], CONFIG_KEYS)),
 
     // Prompt user
     withPrompt(({ operation, scriptHash }) => (
