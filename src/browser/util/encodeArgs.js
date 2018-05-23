fe4174f534a9763fb11c6e657a05a2030cf98f5a
Jeroen Peeters <jeroen.peeters@evance.be>
2018-05-23 23:13:30 +0200

    chore(app): update encodedArgs

--- src/browser/util/encodeArgs.js
+++ src/browser/util/encodeArgs.js
@@ -7,20 +7,15 @@ const encodeArgs = (args) => {
     return args;
   }
 
-  // For now this takes care of strings and numbers and nested Arrays
-  // Also this shouldn't be used when NEO addresses are passed for contracts using CheckWitness.
-  // Until we determine what a good practice for general encoding addresses is
-  // - using unhexlify + reverseHex + toScriptHash or just reverseHex as is used in this function -
-  // but the former is necessary for contracts using CheckWitness
-  // the latter is used from neon-js guides to checkBalance.
-  // If the former can be used for those as well, this is no problem.
+  // We need to determine what a good practice for general encoding addresses is.
+  // For now, this shouldn't be used when NEO addresses are passed for contracts using CheckWitness.
   const encodedArgs = args.map((arg) => {
     const executor = tree`
       lazy ${arg}
         expression ${wallet.isAddress} ${() => u.reverseHex(wallet.getScriptHashFromAddress(arg))}
         expression ${Array.isArray} ${() => encodeArgs(arg)}
         type ${'string'} ${() => u.str2hexstring(arg)}
-        type ${1} ${() => u.int2hex(arg)}
+        type ${'number'} ${() => u.int2hex(arg)}
     `;
     return executor();
   });
