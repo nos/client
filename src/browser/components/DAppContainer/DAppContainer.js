8e3029cd8b9f0869648a9220a661016169d03f23
jarindr <jarindr23@gmail.com>
2018-05-27 03:50:37 +0700

    chore(browser): auto fix linting

--- src/browser/components/DAppContainer/DAppContainer.js
+++ src/browser/components/DAppContainer/DAppContainer.js
@@ -21,11 +21,11 @@ export default class DAppContainer extends React.Component {
     dequeue: func.isRequired,
     empty: func.isRequired,
     closeTab: func.isRequired
-  };
+  }
 
   static defaultProps = {
     className: null
-  };
+  }
 
   componentDidMount() {
     this.webview.addEventListener('console-message', this.handleConsoleMessage);
@@ -83,7 +83,7 @@ export default class DAppContainer extends React.Component {
 
   handleConsoleMessage = (event) => {
     console.log('[DApp]', event.message); // eslint-disable-line no-console
-  };
+  }
 
   handleIPCMessage = (event) => {
     const { channel } = event;
@@ -91,55 +91,55 @@ export default class DAppContainer extends React.Component {
     const args = event.args.slice(1);
 
     this.props.enqueue(this.props.sessionId, { channel, id, args });
-  };
+  }
 
   handlePageTitleUpdated = (event) => {
     this.props.setTabTitle(this.props.sessionId, event.title);
-  };
+  }
 
   handleNavigatingToPage = (event) => {
     this.props.setTabTarget(this.props.sessionId, event.url);
-  };
+  }
 
   handleNavigatedToPage = () => {
     this.props.setTabLoaded(this.props.sessionId, true);
-  };
+  }
 
   handleNavigatedToAnchor = (event) => {
     this.props.setTabTarget(this.props.sessionId, event.url, { leavingPage: false });
-  };
+  }
 
   handleNavigateFailed = (_event) => {
     // TODO: Display an error page or something.  For now, just clear out the loading spinner.
     this.props.setTabLoaded(this.props.sessionId, true);
-  };
+  }
 
   handleNewWindow = (event) => {
     event.preventDefault();
     shell.openExternal(event.url);
-  };
+  }
   handleCloseWindow = () => {
     this.props.closeTab(this.props.sessionId);
-  };
+  }
 
   handleResolve = (request, result) => {
     const { channel, id } = request;
     this.webview.send(`${channel}-success-${id}`, result);
     this.props.dequeue(this.props.sessionId, id);
-  };
+  }
 
   handleReject = (request, message) => {
     const { channel, id } = request;
     this.webview.send(`${channel}-failure-${id}`, message);
     this.props.dequeue(this.props.sessionId, id);
-  };
+  }
 
   registerRef = (el) => {
     this.webview = el;
-  };
+  }
 
   getPreloadPath = () => {
     const publicPath = process.env.NODE_ENV === 'production' ? __dirname : process.env.PUBLIC_PATH;
     return `file:${path.join(publicPath, 'preloadRenderer.js')}`;
-  };
+  }
 }
