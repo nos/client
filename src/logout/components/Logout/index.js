07f32d08c25544858cd54f0775f60915eba7890c
Jarindr Thitadilaka <jarindr23@gmail.com>
2018-05-27 21:44:16 +0700

    feat(browser): empty request queue on logout (#177)
    * feat(browser): remove request queue when logout
    
    * fix from comments
    
    * use compose with props
    
    * refactor to mapDispatchToProps

--- src/logout/components/Logout/index.js
+++ src/logout/components/Logout/index.js
@@ -1,16 +1,28 @@
-import { compose } from 'recompose';
+import { compose, withProps } from 'recompose';
 import { withActions } from 'spunky';
-
+import { connect } from 'react-redux';
 import accountActions from 'shared/actions/accountActions';
-
 import Logout from './Logout';
 import withLogout from '../../hocs/withLogout';
 
+import { emptyAll } from '../../../browser/actions/requestsActions';
+
 const mapActionsToProps = ({ reset }) => ({
-  logout: reset
+  reset
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  emptyAllRequests: () => dispatch(emptyAll())
 });
 
 export default compose(
   withActions(accountActions, mapActionsToProps),
-  withLogout((state, { history }) => history.push('/login'))
+  connect(null, mapDispatchToProps),
+  withLogout((state, { history }) => history.push('/login')),
+  withProps(({ emptyAllRequests, reset }) => ({
+    logout: () => {
+      reset();
+      emptyAllRequests();
+    }
+  }))
 )(Logout);
