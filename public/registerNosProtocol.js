7049f8ca541aa06f2d1f9fafbf173d13e776f7f9
Matt Huggins <matt.huggins@gmail.com>
2018-06-10 18:04:47 -0500

    chore(protocol): extracted helper functions

--- public/registerNosProtocol.js
+++ public/registerNosProtocol.js
@@ -10,29 +10,36 @@ const resolve = require('./resolve');
 
 const { protocol } = electron;
 
+function streamFile(uri, callback) {
+  const contentType = mime.contentType(path.extname(uri.path));
+
+  callback({
+    statusCode: 200,
+    headers: { 'content-type': contentType },
+    data: createReadStream(uri.path)
+  });
+}
+
+async function streamFetch(uri, callback) {
+  const response = await fetch(uri.href);
+
+  // Since the body is being streamed without encoding, we need to strip this header.
+  const validHeaders = omit(response.headers.raw(), 'content-encoding');
+
+  callback({
+    statusCode: response.status,
+    headers: validHeaders,
+    data: response.body
+  });
+}
+
 function registerNosProtocol() {
   protocol.registerStreamProtocol('nos', async (req, callback) => {
     try {
-      const resolvedUrl = url.parse(await resolve(url.parse(req.url)));
+      const uri = url.parse(await resolve(url.parse(req.url)));
+      const stream = uri.protocol === 'file:' ? streamFile : streamFetch;
 
-      if (resolvedUrl.protocol === 'file:') {
-        const contentType = mime.contentType(path.extname(resolvedUrl.path));
-
-        callback({
-          statusCode: 200,
-          headers: { 'content-type': contentType },
-          data: createReadStream(resolvedUrl.path)
-        });
-      } else {
-        const response = await fetch(resolvedUrl.href);
-
-        callback({
-          statusCode: response.status,
-          // Since the body is being streamed without encoding, we need to strip this header.
-          headers: omit(response.headers.raw(), 'content-encoding'),
-          data: response.body
-        });
-      }
+      await stream(uri, callback);
     } catch (error) {
       // console.error(error);
       callback({ error });
