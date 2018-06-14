922b254a9580667111025f85e926d175b9d13d6f
Matt Huggins <matt.huggins@gmail.com>
2018-06-14 08:36:50 -0500

    chore(register): passphrase removal (#235)
    * feat(register): remove passphrase from post-register screen
    
    * chore(register): fix AccountDatum component name
    
    * chore(register): added AccountDetails component tests

---
+++ __tests__/register/components/AccountDetails/AccountDetails.test.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+
+import AccountDetails from 'register/components/AccountDetails/AccountDetails';
+
+describe('<AccountDetails />', () => {
+  const account = {
+    address: 'my-address',
+    key: 'my-key',
+    encryptedKey: 'my-encrypted-key',
+    passphrase: 'my-passphrase'
+  };
+  let wrapper;
+
+  const findDatum = (label) => {
+    return wrapper.find('AccountDatum').findWhere((node) => node.prop('label') === label);
+  };
+
+  beforeEach(() => {
+    wrapper = shallow(<AccountDetails account={account} />);
+  });
+
+  it('renders address', () => {
+    expect(findDatum('Address').prop('value')).toEqual(account.address);
+  });
+
+  it('renders private key', () => {
+    expect(findDatum('Private Key').prop('value')).toEqual(account.key);
+  });
+
+  it('renders encrypted key', () => {
+    expect(findDatum('Encrypted Key').prop('value')).toEqual(account.encryptedKey);
+  });
+
+  it('does not render passphrase', () => {
+    expect(findDatum('Passphrase')).toHaveLength(0);
+  });
+});
