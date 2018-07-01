0ff2aab8dfd7b89b14d2ce40df516917c36cd641
Nikolai Perevozchikov <trueinsider@gmail.com>
2018-07-01 14:26:55 +0300

    lint fix

--- src/renderer/shared/util/encodeArgs.js
+++ src/renderer/shared/util/encodeArgs.js
@@ -10,7 +10,7 @@ const encodeArgs = (args) => {
   // We need to determine what a good practice for general encoding addresses is.
   // For now, this shouldn't be used when NEO addresses are passed for contracts using CheckWitness.
   const encodedArgs = args.map((arg) => {
-    const isFixed8 = input => input instanceof u.Fixed8;
+    const isFixed8 = (input) => input instanceof u.Fixed8;
 
     const executor = tree`
       lazy ${arg}
