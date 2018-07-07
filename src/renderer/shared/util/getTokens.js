89154f7c713c3744c4e6b32ccb8ea23f11b0e9f6
Maurice Dalderup <Maurice_Dalderup@hotmail.com>
2018-07-07 09:54:32 +0200

    chore(getTokens): pr comment - fixed undefined issue

--- src/renderer/shared/util/getTokens.js
+++ src/renderer/shared/util/getTokens.js
@@ -1,20 +1,17 @@
-import { sortBy, values } from 'lodash';
 import fetch from 'node-fetch';
+import { sortBy, values, compact } from 'lodash';
 
 import { TOKENS_URL, NETWORK_MAP } from 'shared/values/config';
 
 export default async function getTokens(net, details = false) {
   const networkKey = NETWORK_MAP[net];
+
   const response = await fetch(TOKENS_URL);
   const tokens = values(await response.json());
-
-  // eslint-disable-next-line
-  return sortBy(tokens, 'symbol').map((token) => {
-    const tokenDetails = token.networks[networkKey];
-    if (tokenDetails) {
-      return details
-        ? { symbol: token.symbol, ...tokenDetails }
-        : tokenDetails.hash;
-    }
-  });
+  const sortedTokens = sortBy(tokens, 'symbol');
+  const networkTokens = compact(sortedTokens.map((token) => ({
+    symbol: token.symbol,
+    ...token.networks[networkKey]
+  })));
+  return networkTokens.map((token) => (details ? token : token.hash));
 }
