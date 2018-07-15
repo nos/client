dd40e610f0dd54400cbcb97b4c81942a64916ee1
Nikolai Perevozchikov <trueinsider@gmail.com>
2018-07-16 01:01:46 +0300

    add tests

--- src/renderer/browser/actions/makeSendActions.js
+++ src/renderer/browser/actions/makeSendActions.js
@@ -6,10 +6,10 @@ import generateDAppActionId from './generateDAppActionId';
 
 export const ID = 'send';
 
-export default function makeSendActions(sessionId, requestId) {
+export default function makeSendActions(sessionId, requestId, call = sendAsset) {
   const id = generateDAppActionId(sessionId, `${ID}-${requestId}`);
 
   return createActions(id, ({ net, asset, amount, receiver, address, wif, remark }) => () => {
-    return sendAsset({ net, asset, amount, receiver, address, wif, remark });
+    return call({ net, asset, amount, receiver, address, wif, remark });
   });
 }
