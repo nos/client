873e911cd19736e79f6fce84b7bc67656f6378b0
Matt Huggins <matt.huggins@gmail.com>
2018-07-29 12:21:27 -0500

    chore(encrypt): extract getPublicKey to testable util function

--- src/renderer/browser/actions/makePublicKeyActions.js
+++ src/renderer/browser/actions/makePublicKeyActions.js
@@ -1,14 +1,9 @@
 import { createActions } from 'spunky';
-import { wallet } from '@cityofzion/neon-js';
 
 import generateDAppActionId from './generateDAppActionId';
+import getPublicKey from '../util/getPublicKey';
 
 export const ID = 'getPublicKey';
-
-const getPublicKey = (wif) => {
-  const account = new wallet.Account(wif);
-  return account.publicKey;
-};
 
 export default function makePublicKeyActions(sessionId, requestId) {
   const id = generateDAppActionId(sessionId, `${ID}-${requestId}`);
