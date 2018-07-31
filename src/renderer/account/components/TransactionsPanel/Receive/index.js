ab44a2ffb812795579c90d78b724a7215e453e29
Matt Huggins <matt.huggins@gmail.com>
2018-07-30 20:58:05 -0500

    feat(account): add receive tab content

--- src/renderer/account/components/TransactionsPanel/Receive/index.js
+++ src/renderer/account/components/TransactionsPanel/Receive/index.js
@@ -1,1 +1,9 @@
-export { default } from './Receive';
+import { withData } from 'spunky';
+
+import authActions from 'login/actions/authActions';
+
+import Receive from './Receive';
+
+const mapAuthDataToProps = ({ address }) => ({ address });
+
+export default withData(authActions, mapAuthDataToProps)(Receive);
