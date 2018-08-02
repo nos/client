57f6216ed8f0191131ad5ff45636b74fb55e10a3
Matt Huggins <matt.huggins@gmail.com>
2018-08-01 23:58:39 -0500

    feat(settings): sidebar navigation

--- src/renderer/settings/components/Settings/Settings.js
+++ src/renderer/settings/components/Settings/Settings.js
@@ -1,19 +1,50 @@
 import React from 'react';
+import { Element, scrollSpy } from 'react-scroll';
 
 import Page from 'shared/components/Page';
 import Panel from 'shared/components/Panel';
 
 import GeneralSettings from '../GeneralSettings';
 import NetworkSettings from '../NetworkSettings';
+import SidebarLink from '../SidebarLink';
 import styles from './Settings.scss';
 
-export default function Settings() {
-  return (
-    <Page className={styles.settings}>
-      <Panel className={styles.panel}>
-        <GeneralSettings />
-        <NetworkSettings />
-      </Panel>
-    </Page>
-  );
+export default class Settings extends React.PureComponent {
+  componentDidMount() {
+    scrollSpy.update();
+  }
+
+  render() {
+    return (
+      <div id="settingsContainer" className={styles.settings}>
+        <Page className={styles.page} id="settingsContainer">
+          <Panel className={styles.panel}>
+            <div className={styles.sidebar}>
+              <ul>
+                <li>
+                  <SidebarLink to="general" containerId="settingsContainer" offset={-24}>
+                    General
+                  </SidebarLink>
+                </li>
+                <li>
+                  <SidebarLink to="network" containerId="settingsContainer" offset={-24}>
+                    Network
+                  </SidebarLink>
+                </li>
+              </ul>
+            </div>
+
+            <div className={styles.content}>
+              <Element name="general">
+                <GeneralSettings />
+              </Element>
+              <Element name="network">
+                <NetworkSettings />
+              </Element>
+            </div>
+          </Panel>
+        </Page>
+      </div>
+    );
+  }
 }
