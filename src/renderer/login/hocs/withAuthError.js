9603590c143859fb7f47e180d78a6ef85bd392f1
Matt Huggins <matt.huggins@gmail.com>
2018-08-05 13:41:46 -0500

    feat(toast): added error toast to login

--- src/renderer/login/hocs/withAuthError.js
+++ src/renderer/login/hocs/withAuthError.js
@@ -1,12 +1,12 @@
 import { compose } from 'recompose';
 import { progressValues } from 'spunky';
 
-import withAlert from 'shared/hocs/withAlert';
+import withErrorToast from 'shared/hocs/withErrorToast';
 import withAuthChange from 'shared/hocs/withAuthChange';
 
 const { FAILED } = progressValues;
 
 export default compose(
-  withAlert(),
-  withAuthChange(FAILED, ({ error }, { alert }) => alert(`Authentication failed: ${error}`))
+  withErrorToast(),
+  withAuthChange(FAILED, (state, props) => props.showToast(`Authentication failed: ${state.error}`))
 );
