f8ef26d44818bdaef3a885a9a6fbe2ad75bcee12
Matt Huggins <matt.huggins@gmail.com>
2018-08-10 17:27:10 -0500

    chore(register): remove account details after registration success

--- src/renderer/register/components/Register/index.js
+++ src/renderer/register/components/Register/index.js
@@ -1,8 +1,14 @@
+import { compose } from 'recompose';
 import { withData } from 'spunky';
+
+import withImmediateReset from 'shared/hocs/withImmediateReset';
 
 import Register from './Register';
 import createAccountActions from '../../actions/createAccountActions';
 
 const mapAccountDataToProps = (account) => ({ account });
 
-export default withData(createAccountActions, mapAccountDataToProps)(Register);
+export default compose(
+  withImmediateReset(createAccountActions),
+  withData(createAccountActions, mapAccountDataToProps)
+)(Register);
