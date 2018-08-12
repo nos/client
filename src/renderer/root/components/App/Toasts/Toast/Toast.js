98149cf353bee5ce88b0559266000d2e960e896b
Matt Huggins <matt.huggins@gmail.com>
2018-08-11 21:54:04 -0500

    chore(toast): add error & info icons

--- src/renderer/root/components/App/Toasts/Toast/Toast.js
+++ src/renderer/root/components/App/Toasts/Toast/Toast.js
@@ -3,6 +3,8 @@ import { func } from 'prop-types';
 
 import toastShape from 'shared/shapes/toastShape';
 import SuccessIcon from 'shared/images/toasts/success.svg';
+import ErrorIcon from 'shared/images/toasts/error.svg';
+import InfoIcon from 'shared/images/toasts/info.svg';
 import { TYPE_SUCCESS, TYPE_ERROR, TYPE_INFO } from 'shared/actions/toastsActions';
 
 import styles from './Toast.scss';
@@ -47,11 +49,18 @@ export default class Toast extends React.PureComponent {
   }
 
   renderIcon = () => {
+    const Icon = this.getIcon();
+    return Icon && <Icon className={styles.icon} />;
+  }
+
+  getIcon = () => {
     switch (this.props.toast.type) {
       case TYPE_SUCCESS:
+        return SuccessIcon;
       case TYPE_ERROR:
+        return ErrorIcon;
       case TYPE_INFO:
-        return <SuccessIcon className={styles.icon} />;
+        return InfoIcon;
       default:
         return null;
     }
