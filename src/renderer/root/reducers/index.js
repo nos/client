51c16f589eb621ae733b4fa05bc742a2639ac3d1
Maurice Dalderup <Maurice.Dalderup@trase.be>
2018-08-13 18:08:24 +0200

    chore(reducers): removed usage react-router-redux

--- src/renderer/root/reducers/index.js
+++ src/renderer/root/reducers/index.js
@@ -1,5 +1,4 @@
 import { combineReducers } from 'redux';
-import { routerReducer } from 'react-router-redux';
 import { reducer as spunkyReducer } from 'spunky';
 
 import browserReducer from 'browser/reducers/browserReducer';
@@ -9,7 +8,6 @@ import toastsReducer from 'shared/reducers/toastsReducer';
 import dialogsReducer from './dialogsReducer';
 
 export default combineReducers({
-  router: routerReducer,
   spunky: spunkyReducer,
   dialogs: dialogsReducer,
   browser: browserReducer,
