477f49af1dce45ff75185bcb8ce841494d3c1866
Matt Huggins <matt.huggins@gmail.com>
2018-08-18 10:24:30 -0500

    chore(register): add AccountDatum component tests

--- __tests__/setupTests.js
+++ __tests__/setupTests.js
@@ -1,8 +1,24 @@
 import path from 'path';
 import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
+import { noop } from 'lodash';
 
 configure({ adapter: new Adapter() });
 
 // Polyfill global defined by electron-webpack.
 global.__static = path.join(__dirname, '..', 'static'); // eslint-disable-line no-underscore-dangle
+
+// Polyfill window prompts to always confirm.  Needed for react-copy-to-clipboard to work.
+global.prompt = () => true;
+
+// Polyfill text selection functionality.  Needed for react-copy-to-clipboard to work.  Can remove
+// this once https://github.com/jsdom/jsdom/issues/317 is implemented.
+const getSelection = () => ({
+  rangeCount: 0,
+  addRange: noop,
+  getRangeAt: noop,
+  removeAllRanges: noop
+});
+
+window.getSelection = getSelection;
+document.getSelection = getSelection;
