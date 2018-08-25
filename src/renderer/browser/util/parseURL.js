ac230a0ae75fa88ac24a4f09dfea9debb92cffa6
Maurice Dalderup <Maurice_Dalderup@hotmail.com>
2018-08-25 20:52:43 +0200

    chore(redirect): fixed faulty redirect

--- src/renderer/browser/util/parseURL.js
+++ src/renderer/browser/util/parseURL.js
@@ -1,38 +1,30 @@
-import { drop, some, startsWith, split, trim, trimEnd, endsWith } from 'lodash';
+import { defaultTo, noop, filter, isEmpty, forEach, reduce, some, startsWith, split, trim, trimEnd, endsWith } from 'lodash';
 
-const TLDs = ['.neo', '.neoo'];
 const protocols = {
-  http: 'http:',
-  https: 'https:',
-  nos: 'nos:'
+  http: {
+    tld: noop,
+    protocol: 'http:'
+  },
+  https: {
+    tld: noop,
+    protocol: 'https:'
+  },
+  nos: {
+    tld: '.neo',
+    protocol: 'nos:'
+  }
 };
-const defaultProtocol = protocols.https;
-
-function determineHttp(urlQuery) {
-  return urlQuery.protocol === protocols.nos
-    ? Object.assign(urlQuery, { protocol: defaultProtocol })
-    : urlQuery;
-}
-
-// No protocol?         --> append protocol depending on TLD
-
-// custom TLD?          --> modify protocol
-// No custom TLD?       --> don't modify tld/protocol
 
 export default function parseURL(query) {
   try {
     const trimmedQuery = trimEnd(trim(query), '/');
-    const containsCustomTLD = some(TLDs, (tld) => endsWith(trimmedQuery, tld));
-    const containsProtocol = some(protocols, (protocol) => startsWith(trimmedQuery, protocol));
 
-    const formattedQuery = !containsProtocol ? `${defaultProtocol}//${trimmedQuery}` : trimmedQuery;
-    console.log(formattedQuery);
+    const tld = filter(protocols, (protocol) => endsWith(trimmedQuery, protocol.tld));
+    const tldToApply = isEmpty(tld) ? protocols.https : tld[0];
 
-    const urlQuery = new URL(trimmedQuery);
-    console.log('urlQuery', urlQuery);
-    return containsCustomTLD
-      ? Object.assign(urlQuery, { protocol: protocols.nos })
-      : determineHttp(urlQuery);
+    return some(protocols, (protocol) => startsWith(trimmedQuery, protocol.protocol))
+      ? new URL(trimmedQuery)
+      : new URL(`${tldToApply.protocol}//${trimmedQuery}`);
   } catch (e) {
     console.log('error', e);
   }
