a5e8f690c13152a8ab3e176deda722d18d67e864
Matt Huggins <matt.huggins@gmail.com>
2018-08-25 14:40:50 -0500

    chore(tests): make container queries more resilient

--- __tests__/renderer/shared/components/Forms/LabeledInput/index.test.js
+++ __tests__/renderer/shared/components/Forms/LabeledInput/index.test.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { mount } from 'enzyme';
 
 import LabeledInput from 'shared/components/Forms/LabeledInput';
+import Label from 'shared/components/Forms/Label';
+import Input from 'shared/components/Forms/Input';
 
 const mountContainer = (props = {}) => {
   return mount(<LabeledInput {...props} />);
@@ -10,7 +12,7 @@ const mountContainer = (props = {}) => {
 describe('<LabeledInput />', () => {
   it('renders a label', () => {
     const wrapper = mountContainer({ id: 'name', label: 'Name' });
-    const label = wrapper.find('Label');
+    const label = wrapper.find(Label);
     expect(label.exists()).toBe(true);
     expect(label.props()).toEqual(expect.objectContaining({ label: 'Name', htmlFor: 'name' }));
   });
@@ -18,7 +20,7 @@ describe('<LabeledInput />', () => {
   it('renders an input', () => {
     const inputProps = { id: 'pw', type: 'password', defaultValue: 'foo' };
     const wrapper = mountContainer({ label: 'Password', ...inputProps });
-    const input = wrapper.find('Input');
+    const input = wrapper.find(Input);
     expect(input.exists()).toBe(true);
     expect(input.props()).toEqual(expect.objectContaining(inputProps));
   });
