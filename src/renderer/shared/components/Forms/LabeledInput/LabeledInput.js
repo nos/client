9691dcb3f3c4572046f5d47eab0efb9d55b1e59a
Matt Huggins <matt.huggins@gmail.com>
2018-09-01 21:59:15 -0500

    feat(inputs): pass forwardedRef from LabeledInput/LabeledSelect to Input/Select

--- src/renderer/shared/components/Forms/LabeledInput/LabeledInput.js
+++ src/renderer/shared/components/Forms/LabeledInput/LabeledInput.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import classNames from 'classnames';
-import { string, node } from 'prop-types';
+import { string, func, node } from 'prop-types';
 
 import Label from '../Label';
 import Input from '../Input';
@@ -8,21 +8,23 @@ import styles from './LabeledInput.scss';
 
 export default class LabeledInput extends React.PureComponent {
   static propTypes = {
+    forwardedRef: func,
     id: string.isRequired,
     label: node.isRequired,
     labelClass: string
   };
 
   static defaultProps = {
+    forwardedRef: null,
     labelClass: null
   };
 
   render() {
-    const { id, label, labelClass, ...passDownProps } = this.props;
+    const { id, label, labelClass, forwardedRef, ...passDownProps } = this.props;
 
     return (
       <Label htmlFor={id} label={label} className={classNames(styles.labeledInput, labelClass)}>
-        <Input id={id} {...passDownProps} />
+        <Input id={id} ref={forwardedRef} {...passDownProps} />
       </Label>
     );
   }
--- src/renderer/shared/components/Forms/LabeledSelect/LabeledSelect.js
+++ src/renderer/shared/components/Forms/LabeledSelect/LabeledSelect.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import classNames from 'classnames';
-import { string, node } from 'prop-types';
+import { string, func, node } from 'prop-types';
 
 import Label from '../Label';
 import Select from '../Select';
@@ -8,21 +8,23 @@ import styles from './LabeledSelect.scss';
 
 export default class LabeledSelect extends React.PureComponent {
   static propTypes = {
+    forwardedRef: func,
     id: string.isRequired,
     label: node.isRequired,
     labelClass: string
   };
 
   static defaultProps = {
+    forwardedRef: null,
     labelClass: null
   };
 
   render() {
-    const { id, label, labelClass, ...passDownProps } = this.props;
+    const { id, label, labelClass, forwardedRef, ...passDownProps } = this.props;
 
     return (
       <Label htmlFor={id} label={label} className={classNames(styles.labeledSelect, labelClass)}>
-        <Select id={id} {...passDownProps} />
+        <Select id={id} ref={forwardedRef} {...passDownProps} />
       </Label>
     );
   }
