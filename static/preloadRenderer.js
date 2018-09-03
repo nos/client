adb1b019f16ced14fd0b2aa8149aa6b981ca6e3b
Matt Huggins <matt.huggins@gmail.com>
2018-09-03 13:09:02 -0500

    feat(api): added NOS.ASSETS constant

--- static/preloadRenderer.js
+++ static/preloadRenderer.js
@@ -22,6 +22,11 @@ function createDelegate(channel) {
   });
 }
 
+const ASSETS = {
+  NEO: 'c56f33fc6ecfcd0c225c4ab356fee59390af8560be0e930faebe74a6daff7c9b',
+  GAS: '602c79718b16e442de58778e148d0b1084e3b2dffd5de6b7b16cee7969282de7'
+};
+
 const V1 = {
   // No persmissions required
   getAddress: createDelegate('getAddress'),
@@ -38,5 +43,5 @@ const V1 = {
 process.once('loaded', () => {
   // Be careful not to expose any functionality or APIs that could compromise the user's system.
   // For example, don't directly expose core Electron (even IPC) or node.js modules.
-  window.NOS = { V1 };
+  window.NOS = { ASSETS, V1 };
 });
