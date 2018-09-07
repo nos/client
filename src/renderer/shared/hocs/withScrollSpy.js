1952505241ffafd80c05f61645152fea514f2173
Matt Huggins <matt.huggins@gmail.com>
2018-09-06 21:00:14 -0500

    feat(select): treat LoginLayout as scrolling region for dropdowns

--- src/renderer/shared/hocs/withScrollSpy.js
+++ src/renderer/shared/hocs/withScrollSpy.js
@@ -1,27 +1,28 @@
+/* eslint-disable react/no-find-dom-node */
+
 import React from 'react';
+import ReactDOM from 'react-dom';
 
 import { defaultScrollSpy } from 'shared/lib/scrollSpy';
 
 export default function withScrollSpy(Component) {
   return class ScrollSpyComponent extends React.PureComponent {
+    component = React.createRef();
+
     componentDidMount() {
-      this.component.addEventListener('scroll', this.handleScroll);
+      ReactDOM.findDOMNode(this.component.current).addEventListener('scroll', this.handleScroll);
     }
 
     componentWillUnmount() {
-      this.component.removeEventListener('scroll', this.handleScroll);
+      ReactDOM.findDOMNode(this.component.current).removeEventListener('scroll', this.handleScroll);
     }
 
     render() {
-      return <Component {...this.props} ref={this.registerRef} />;
+      return <Component {...this.props} ref={this.component} />;
     }
 
     handleScroll = (event) => {
       defaultScrollSpy.publish(event);
-    }
-
-    registerRef = (el) => {
-      this.component = el;
     }
   };
 }
