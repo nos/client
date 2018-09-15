e827144a91331a53f6c4d3a8cd705d05e8d7fc4e
Matt Huggins <matt.huggins@gmail.com>
2018-09-14 20:33:04 -0500

    feat(balance): update balances on new blocks

--- __tests__/renderer/root/components/Layout/AuthenticatedLayout/AuthenticatedLayout.test.js
+++ __tests__/renderer/root/components/Layout/AuthenticatedLayout/AuthenticatedLayout.test.js
@@ -2,12 +2,16 @@ import React from 'react';
 import { mount } from 'enzyme';
 import { MemoryRouter } from 'react-router-dom';
 
-import { provideState } from 'testHelpers';
+import { provideState, spunkyKey, mockSpunkyLoaded } from 'testHelpers';
 
 import AuthenticatedLayout from 'root/components/AuthenticatedLayout';
 import { EXTERNAL } from 'browser/values/browserValues';
 
 const initialState = {
+  [spunkyKey]: {
+    currentNetwork: mockSpunkyLoaded('MainNet'),
+    auth: mockSpunkyLoaded({ address: 'ALfnhLg7rUyL6Jr98bzzoxz5J7m64fbR4s' })
+  },
   browser: {
     currentNetwork: 'MainNet',
     activeSessionId: 'tab-1',
