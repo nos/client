754d59b77c6543f6b563a50795a49f207c2b0eb4
Maurice Dalderup <Maurice_Dalderup@hotmail.com>
2018-09-21 19:49:52 +0200

    chore(balances): replaced token balances strategy

--- src/renderer/shared/util/getBalances.js
+++ src/renderer/shared/util/getBalances.js
@@ -4,14 +4,19 @@ import { api, rpc, wallet } from '@cityofzion/neon-js';
 import getTokens from './getTokens';
 import { GAS, NEO, ASSETS } from '../values/assets';
 
-async function getTokenBalance(endpoint, token, address) {
+async function getTokenBalance(endpoint, tokens, address) {
   try {
-    const response = await api.nep5.getToken(endpoint, token.scriptHash, address);
-    const balance = (response.balance || 0).toString();
+    const tokenHashes = tokens.map((token) => token.scriptHash);
+    const response = await api.nep5.getTokenBalances(endpoint, tokenHashes, address);
+    const response2 = await api.nep5.getToken(endpoint, tokens[0].scriptHash, address);
+    // const balance = (response.balance || 0).toString();
 
-    return {
-      [token.scriptHash]: { ...token, ...response, balance }
-    };
+    console.log(response);
+    console.log(response2);
+
+    // return {
+    //   [token.scriptHash]: { ...token, ...response, balance }
+    // };
   } catch (err) {
     // invalid scriptHash
     return {};
@@ -38,10 +43,9 @@ export default async function getBalances({ net, address }) {
     throw new Error(`Invalid address: "${address}"`);
   }
 
-  // token balances - // TODO use getTokenBalances to avoid multiple api calls
-  const promises = (await getTokens(net)).map((token) => {
-    return getTokenBalance(endpoint, token, address);
-  });
+  // Get token scriptHashes
+  const tokens = await getTokens(net);
+  const allTokens = getTokenBalance(endpoint, tokens, address);
 
   // asset balances
   promises.unshift(getAssetBalances(endpoint, address));
