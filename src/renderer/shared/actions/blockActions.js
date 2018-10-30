127e5a9109a6c72826368f68ac2177848e2f7b41
Matt Huggins <matt.huggins@gmail.com>
2018-10-29 20:25:00 -0500

    chore(rpc): filter out unreliable nodes (#683)

--- src/renderer/shared/actions/blockActions.js
+++ src/renderer/shared/actions/blockActions.js
@@ -1,10 +1,12 @@
-import { api, rpc } from '@cityofzion/neon-js';
+import { rpc } from '@cityofzion/neon-js';
 import { createActions } from 'spunky';
+
+import getRPCEndpoint from 'util/getRPCEndpoint';
 
 export const ID = 'block';
 
 export default createActions(ID, ({ net }) => async () => {
-  const endpoint = await api.getRPCEndpointFrom({ net }, api.neoscan);
+  const endpoint = await getRPCEndpoint(net);
   const client = new rpc.RPCClient(endpoint);
   const height = await client.getBlockCount();
   return client.getBlock(height - 1);
