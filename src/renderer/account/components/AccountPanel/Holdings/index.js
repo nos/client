00647048fbdf3237ca8c1038364dba69b14ee7cf
Matt Huggins <matt.huggins@gmail.com>
2018-10-30 11:52:09 -0500

    feat(balance): sort holdings by precidence

--- src/renderer/account/components/AccountPanel/Holdings/index.js
+++ src/renderer/account/components/AccountPanel/Holdings/index.js
@@ -1,1 +1,28 @@
-export { default } from './Holdings';
+import { withProps } from 'recompose';
+import { sortBy } from 'lodash';
+
+import { NOS, NEO, GAS } from 'shared/values/assets';
+
+import Holdings from './Holdings';
+
+/**
+ * Sort by:
+ * 1. preferred tokens
+ * 2. token symbol (ascending)
+ */
+const customSort = (balances, preferredOrder) => {
+  return sortBy(balances, (token) => {
+    const precidence = preferredOrder.findIndex((v) => v === token.scriptHash);
+
+    return [
+      precidence < 0 ? Infinity : precidence,
+      token.symbol
+    ];
+  });
+};
+
+const sortBalances = ({ balances }) => ({
+  balances: customSort(balances, [NOS, NEO, GAS])
+});
+
+export default withProps(sortBalances)(Holdings);
