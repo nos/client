914cb2cfecbc6d5b7bbd667fdee6cb6fa3a59aed
Maurice Dalderup <Maurice_Dalderup@hotmail.com>
2018-12-09 13:58:59 +0100

    chore(store): removed default store items

--- src/renderer/root/store/configureStore.js
+++ src/renderer/root/store/configureStore.js
@@ -9,21 +9,7 @@ import { identity } from 'lodash';
 import createRootReducer from '../reducers';
 
 export default function configureStore(history) {
-  const initialState = {
-    spunky: {
-      auth: {
-        batch: false,
-        progress: 'LOADED',
-        rollbackProgress: 'LOADED',
-        loadedCount: 1,
-        data: {
-          wif: 'KxDgvEKzgSBPPfuVfw67oPQBSjidEiqTHURKSDL1R7yGaGYAeYnr',
-          address: 'AK2nJJpJr6o664CWJKi1QRXjqeic2zRp8y'
-        },
-        error: null
-      }
-    }
-  };
+  const initialState = {};
   const sagaMiddleware = createSagaMiddleware();
 
   const middleware = [thunk, sagaMiddleware, routerMiddleware(history)];
@@ -34,11 +20,7 @@ export default function configureStore(history) {
       : require('redux-devtools-extension').composeWithDevTools;
 
   const enhancers = composeEnhancers(applyMiddleware(...middleware));
-  const store = createStore(
-    createRootReducer(history),
-    initialState,
-    enhancers
-  );
+  const store = createStore(createRootReducer(history), initialState, enhancers);
 
   if (module.hot) {
     module.hot.accept('../reducers', () => {
