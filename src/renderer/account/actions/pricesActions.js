e5d54e9b7828e90f0ba5b28f568ed180ade1ff71
Maurice Dalderup <Maurice_Dalderup@hotmail.com>
2019-01-12 14:02:54 +0100

    feat(account): migrate to coingecko

--- src/renderer/account/actions/pricesActions.js
+++ src/renderer/account/actions/pricesActions.js
@@ -1,27 +1,52 @@
 import fetch from 'node-fetch';
 import { createActions } from 'spunky';
-import { reduce } from 'lodash';
+import { reduce, map } from 'lodash';
+import qs from 'qs';
+import CoinGecko from 'coingecko-api';
+
+const coinGeckoClient = new CoinGecko();
 
 function mapPrices(tickers, currency) {
-  return reduce(tickers, (mapping, ticker) => ({
-    ...mapping,
-    [ticker.symbol]: parseFloat(ticker[`price_${currency.toLowerCase()}`])
-  }), {});
+  return reduce(
+    tickers,
+    (mapping, ticker) => ({
+      ...mapping,
+      [ticker.symbol]: parseFloat(ticker[`price_${currency.toLowerCase()}`])
+    }),
+    {}
+  );
 }
 
-async function getPrices(currency) {
-  const url = `https://api.coinmarketcap.com/v1/ticker/?limit=0&convert=${currency.toLowerCase()}`;
-
-  const response = await fetch(url);
-  const data = await response.json();
-
-  if (data.error) {
-    throw new Error(data.error);
+async function getPrices(currency, balances) {
+  const coinListResult = await coinGeckoClient.coins.list();
+  if (!coinListResult.success) {
+    throw new Error(coinListResult.message);
   }
 
-  return mapPrices(data, currency);
+  try {
+    const balancesArray = Object.keys(balances).map((key) => balances[key]);
+    const ids = coinListResult.data.reduce((accum, coin) => {
+      if (balancesArray.find((balance) => balance.name.toLowerCase() === coin.name.toLowerCase())) {
+        console.log(coin.id);
+        accum.push(coin.id);
+      }
+      return accum;
+    }, []);
+
+    const coinPricesResult = await coinGeckoClient.coins.markets({
+      vs_currency: currency.toLowerCase(),
+      ids
+    });
+    if (!coinPricesResult.success) {
+      throw new Error(coinPricesResult.message);
+    }
+
+    return mapPrices(coinPricesResult, currency);
+  } catch (e) {
+    console.log(e);
+  }
 }
 
 export const ID = 'prices';
 
-export default createActions(ID, ({ currency }) => () => getPrices(currency));
+export default createActions(ID, ({ currency, balances }) => () => getPrices(currency, balances));
