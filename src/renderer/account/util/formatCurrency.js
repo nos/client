2b5b7063110333fc4bec8542a1c888c88ecbf48e
Matt Huggins <matt.huggins@gmail.com>
2019-01-12 07:43:59 -0700

    feat(account): send panel token/currency conversion (#742)
    * chore(style): reference color variable
    
    * feat(account): send panel token/currency conversion
    
    * feat(account): ConversionInput component tests
    
    * feat(account): ConversionInput container tests
    
    * feat(input): support custom children
    
    * feat(account): truncate decimals on asset change
    
    * chore(send): added disclaimer

--- src/renderer/account/util/formatCurrency.js
+++ src/renderer/account/util/formatCurrency.js
@@ -1,14 +1,15 @@
 import format from 'format-currency';
+import { mapValues } from 'lodash';
 
-import { CAD, EUR, GBP, USD, DEFAULT_CURRENCY } from 'shared/values/currencies';
+import { CAD, EUR, GBP, USD, SYMBOLS, DEFAULT_CURRENCY } from 'shared/values/currencies';
 
 // Refer to https://www.npmjs.com/package/format-currency#usage when adding new currencies.
-const FORMAT_OPTIONS = {
-  [CAD]: { code: CAD, format: '%s%v', symbol: '$' },
-  [EUR]: { code: EUR, format: '%s%v', symbol: '€', locale: 'en-GB' },
-  [GBP]: { code: GBP, format: '%s%v', symbol: '£', locale: 'en-GB' },
-  [USD]: { code: USD, format: '%s%v', symbol: '$' }
-};
+const FORMAT_OPTIONS = mapValues({
+  [CAD]: { format: '%s%v' },
+  [EUR]: { format: '%s%v', locale: 'en-GB' },
+  [GBP]: { format: '%s%v', locale: 'en-GB' },
+  [USD]: { format: '%s%v' }
+}, (options, code) => ({ ...options, code, symbol: SYMBOLS[code] }));
 
 function getFormatOptions(currency) {
   return FORMAT_OPTIONS[currency] || FORMAT_OPTIONS[DEFAULT_CURRENCY];
