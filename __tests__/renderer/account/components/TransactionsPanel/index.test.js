fe349f3a7dbe804333008922105b775248eb57f9
Maurice Dalderup <Maurice_Dalderup@hotmail.com>
2019-01-27 18:39:06 +0100

    chore(tests): adjust tests

--- __tests__/renderer/account/components/TransactionsPanel/index.test.js
+++ __tests__/renderer/account/components/TransactionsPanel/index.test.js
@@ -5,6 +5,9 @@ import { provideStore, createStore, spunkyKey, mockSpunkyLoaded } from 'testHelp
 
 import TransactionsPanel from 'account/components/TransactionsPanel';
 import Send from 'account/components/TransactionsPanel/Send';
+
+import Transactions from 'account/components/TransactionsPanel/Transactions';
+
 import Receive from 'account/components/TransactionsPanel/Receive';
 import IconTab from 'account/components/TransactionsPanel/IconTab';
 import { NEO } from 'shared/values/assets';
@@ -20,37 +23,42 @@ const initialState = {
 };
 
 const defaultProps = {
-  balances: {
-    [NEO]: {
-      scriptHash: NEO,
-      name: 'NEO',
-      symbol: 'NEO',
-      decimals: 0,
-      balance: '0'
-    }
-  },
-  prices: {
-    [NEO]: 7.65
+  [spunkyKey]: {
+    balances: mockSpunkyLoaded({
+      [NEO]: {
+        scriptHash: NEO,
+        name: 'NEO',
+        symbol: 'NEO',
+        decimals: 0,
+        balance: '0'
+      }
+    }),
+    prices: mockSpunkyLoaded({
+      [NEO]: 7.65
+    })
   }
 };
 
 const mountContainer = (props = {}) => {
-  return mount(provideStore(
-    <TransactionsPanel {...defaultProps} {...props} />,
-    createStore(initialState)
-  ));
+  const store = createStore(initialState);
+  return mount(provideStore(<TransactionsPanel {...defaultProps} {...props} />, store));
 };
 
 describe('<TransactionsPanel />', () => {
   it('renders the Send tab contents', () => {
     const wrapper = mountContainer();
+
     expect(wrapper.find(Send).exists()).toBe(true);
+    expect(wrapper.find(Transactions).exists()).toBe(false);
     expect(wrapper.find(Receive).exists()).toBe(false);
   });
 
   it('switches to the Receive tab', () => {
     const wrapper = mountContainer();
-    wrapper.find(IconTab).filterWhere((el) => el.text() === 'Receive').simulate('click');
+    wrapper
+      .find(IconTab)
+      .filterWhere((el) => el.text() === 'Receive')
+      .simulate('click');
     expect(wrapper.find(Send).exists()).toBe(false);
     expect(wrapper.find(Receive).exists()).toBe(true);
   });
