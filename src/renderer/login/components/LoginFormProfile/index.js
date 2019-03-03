556f89491765cbbdf603948c4e84e97ff1779a1d
Maurice Dalderup <Maurice_Dalderup@hotmail.com>
2019-03-03 14:53:13 +0100

    chore(feat): added remember me

--- src/renderer/login/components/LoginFormProfile/index.js
+++ src/renderer/login/components/LoginFormProfile/index.js
@@ -1,6 +1,12 @@
 import { withRouter } from 'react-router-dom';
 import { compose, withState } from 'recompose';
-import { withData, withProgressComponents, alreadyLoadedStrategy, progressValues } from 'spunky';
+import {
+  withData,
+  withProgressComponents,
+  alreadyLoadedStrategy,
+  progressValues,
+  withActions
+} from 'spunky';
 
 import withInitialCall from 'shared/hocs/withInitialCall';
 import Loading from 'shared/components/Loading';
@@ -8,31 +14,49 @@ import Loading from 'shared/components/Loading';
 import { getProfiles } from 'register/actions/storeProfileActions';
 
 import LoginFormProfile from './LoginFormProfile';
+import previousAuthActions, { writePreviousAuthActions } from '../../actions/previousAuthActions';
 import withLogin from '../../hocs/withLogin';
 
 const { LOADING } = progressValues;
 
 const mapProfileActionsToProps = ({ profiles }) => ({ profiles });
+const mapPreviousAuthActionsToProps = (actions) => ({
+  setLastLogin: (data) => actions.call(data)
+});
+
+const mapPreviousAuthDataToProps = (data) => ({
+  encryptedWIF: data && data.encryptedWIF
+});
 
 export default compose(
   withInitialCall(getProfiles),
-  withProgressComponents(
-    getProfiles,
-    {
-      [LOADING]: Loading
-    },
-    {
-      strategy: alreadyLoadedStrategy
-    }
-  ),
+  withInitialCall(previousAuthActions),
+
+  ...[getProfiles, previousAuthActions].map((actions) => {
+    return withProgressComponents(
+      actions,
+      {
+        [LOADING]: Loading
+      },
+      {
+        strategy: alreadyLoadedStrategy
+      }
+    );
+  }),
+
   withData(getProfiles, mapProfileActionsToProps),
+  withData(previousAuthActions, mapPreviousAuthDataToProps),
 
   withState(
     'currentProfile',
     'setCurrentProfile',
-    ({ profiles }) => profiles && profiles[0].encryptedKey
+    ({ encryptedWIF, profiles }) => encryptedWIF || (profiles && profiles[0].encryptedKey)
   ),
   withState('passphrase', 'setPassphrase', ''),
+
+  // store encryptedWIF on login so we can quickly authenticate again next time the app launches
+  withActions(writePreviousAuthActions, mapPreviousAuthActionsToProps),
+  withLogin((data, props) => props.setLastLogin({ encryptedWIF: props.currentProfile })),
 
   // redirect on login
   withRouter,
