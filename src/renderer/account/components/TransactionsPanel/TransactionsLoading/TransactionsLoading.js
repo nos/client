a43962434681e7f0bf61a4500cb51751db849ed7
Maurice Dalderup <Maurice_Dalderup@hotmail.com>
2019-03-10 13:58:54 +0100

    chore(txPanel): filter tokenlist to only contain tokens you have (#917)
    + prioritized nOS in the list order

--- src/renderer/account/components/TransactionsPanel/TransactionsLoading/TransactionsLoading.js
+++ src/renderer/account/components/TransactionsPanel/TransactionsLoading/TransactionsLoading.js
@@ -27,11 +27,27 @@ const TABS = {
 const initialState = {
   balances: {
     c56f33fc6ecfcd0c225c4ab356fee59390af8560be0e930faebe74a6daff7c9b: {
-      scriptHash: 'c56f33fc6ecfcd0c225c4ab356fee59390af8560be0e930faebe74a6daff7c9b',
       name: 'NEO',
       symbol: 'NEO',
       decimals: 0,
-      balance: '12'
+      scriptHash: 'c56f33fc6ecfcd0c225c4ab356fee59390af8560be0e930faebe74a6daff7c9b',
+      balance: '0'
+    },
+    '602c79718b16e442de58778e148d0b1084e3b2dffd5de6b7b16cee7969282de7': {
+      name: 'GAS',
+      symbol: 'GAS',
+      decimals: 8,
+      scriptHash: '602c79718b16e442de58778e148d0b1084e3b2dffd5de6b7b16cee7969282de7',
+      balance: '0'
+    },
+    c9c0fc5a2b66a29d6b14601e752e6e1a445e088d: {
+      name: 'nOS',
+      symbol: 'NOS',
+      scriptHash: 'c9c0fc5a2b66a29d6b14601e752e6e1a445e088d',
+      decimals: 8,
+      totalSupply: 299625982,
+      image: 'https://rawgit.com/CityOfZion/neo-tokens/master/assets/svg/nos.svg',
+      balance: '0'
     }
   },
   prices: {
