cb763969c879518ceaebcb010de167aed52c213e
Maurice Dalderup <Maurice_Dalderup@hotmail.com>
2019-04-05 12:11:29 +0200

    feat(theme): added logical theming support in settings

--- src/renderer/settings/components/GeneralSettings/index.js
+++ src/renderer/settings/components/GeneralSettings/index.js
@@ -9,11 +9,13 @@ import { withSuccessToast, withErrorToast } from 'shared/hocs/withToast';
 import GeneralSettings from './GeneralSettings';
 import currencyActions, { setCurrency } from '../../actions/currencyActions';
 import feeActions, { setFee } from '../../actions/feeActions';
+import themeActions, { setTheme } from '../../actions/themeActions';
 
 const { LOADED, FAILED } = progressValues;
 
 const mapCurrencyDataToProps = (currency) => ({ currency });
 const mapFeeDataToProps = (fee) => ({ fee });
+const mapThemeDataToProps = (theme) => ({ theme });
 
 const mapCurrencyActionsToProps = (actions) => ({
   setCurrency: actions.call
@@ -23,12 +25,19 @@ const mapFeeActionsToProps = (actions) => ({
   setFee: actions.call
 });
 
+const mapThemeActionsToProps = (actions) => ({
+  setTheme: actions.call
+});
+
 export default compose(
   withActions(setCurrency, mapCurrencyActionsToProps),
   withData(currencyActions, mapCurrencyDataToProps),
 
   withActions(setFee, mapFeeActionsToProps),
   withData(feeActions, mapFeeDataToProps),
+
+  withActions(setTheme, mapThemeActionsToProps),
+  withData(themeActions, mapThemeDataToProps),
 
   withReset(balanceWithPricesActions, 'currency'),
 
@@ -39,12 +48,18 @@ export default compose(
   withProgressChange(setFee, LOADED, (state, props) => {
     props.showSuccessToast('Settings successfully updated');
   }),
+  withProgressChange(setTheme, LOADED, (state, props) => {
+    props.showSuccessToast('Settings successfully updated');
+  }),
 
   withErrorToast(),
   withProgressChange(setCurrency, FAILED, (state, props) => {
     props.showErrorToast(`Error updating settings: ${state.error}`);
   }),
   withProgressChange(setFee, FAILED, (state, props) => {
+    props.showErrorToast(`Error updating settings: ${state.error}`);
+  }),
+  withProgressChange(setTheme, FAILED, (state, props) => {
     props.showErrorToast(`Error updating settings: ${state.error}`);
   })
 )(GeneralSettings);
