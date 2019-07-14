25780831abad6687dced66c077be0c367075bf6b
Maurice Dalderup <Maurice_Dalderup@hotmail.com>
2019-07-14 21:41:16 +0200

    chore(auth): further cleanup

--- src/renderer/account/components/Management/Account/Account.js
+++ src/renderer/account/components/Management/Account/Account.js
@@ -7,41 +7,34 @@ import KeyChainIcon from 'shared/images/account/keychain.svg';
 import styles from './Account.scss';
 import EncryptedInput from '../EncryptedInput';
 
-export default class Account extends React.PureComponent {
-  static propTypes = {
-    className: string,
-    encryptedMnemonic: string,
-    secretWord: string.isRequired
-  };
-
-  static defaultProps = {
-    className: null,
-    encryptedMnemonic: ''
-  };
-
-  render() {
-    const { className, encryptedMnemonic, secretWord } = this.props;
-
-    return (
-      <div className={classNames(styles.account, className)}>
-        {this.renderInfo()}
-        <EncryptedInput title="Secret Words" secretWord={secretWord} encryptedData={encryptedMnemonic} />
-      </div>
-    );
-  }
-
-  // TODO move to function component
-  renderInfo = () => (
-    <div className={styles.left}>
-      <div className={styles.icon}>
-        <KeyChainIcon />
-      </div>
-      <div>
-        <div className={styles.title}>
-          Keychain
-        </div>
-        <div className={styles.subtitle}>Secret Words</div>
-      </div>
+const Info = () => (
+  <div className={styles.left}>
+    <div className={styles.icon}>
+      <KeyChainIcon />
     </div>
-  )
-}
+    <div>
+      <div className={styles.title}>
+        Keychain
+      </div>
+      <div className={styles.subtitle}>Secret Words</div>
+    </div>
+  </div>
+);
+
+const Account = ({ className, encryptedMnemonic, secretWord }) => {
+  return (
+    <div className={classNames(styles.account, className)}>
+      <Info />
+      <EncryptedInput title="Secret Words" secretWord={secretWord} encryptedData={encryptedMnemonic} />
+    </div>
+  );
+};
+
+
+Account.propTypes = {
+  className: string.isRequired,
+  encryptedMnemonic: string.isRequired,
+  secretWord: string.isRequired
+};
+
+export default Account;
