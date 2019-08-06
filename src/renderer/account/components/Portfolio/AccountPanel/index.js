dc74a410fd0080092aefb357f0dc2918ff02b22f
Maurice Dalderup <Maurice_Dalderup@hotmail.com>
2019-08-06 20:55:49 +0200

    chore(wallet): adjust hoc name

--- src/renderer/account/components/Portfolio/AccountPanel/index.js
+++ src/renderer/account/components/Portfolio/AccountPanel/index.js
@@ -2,13 +2,13 @@ import { compose } from 'recompose';
 import { withData } from 'spunky';
 
 import currencyActions from 'settings/actions/currencyActions';
-import withActiveAccount from 'shared/hocs/withActiveAccount';
+import withActiveWallet from 'shared/hocs/withActiveWallet';
 
 import AccountPanel from './AccountPanel';
 
 const mapCurrencyDataToProps = (currency) => ({ currency });
 
 export default compose(
-  withActiveAccount(),
+  withActiveWallet(),
   withData(currencyActions, mapCurrencyDataToProps)
 )(AccountPanel);
