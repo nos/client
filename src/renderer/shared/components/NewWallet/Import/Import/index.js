c9d2cd174b4ce06eafa25b9bbfcfbfdf745bd5cc
Maurice Dalderup <Maurice_Dalderup@hotmail.com>
2019-10-12 20:50:29 +0200

    chore(app): fix import

--- src/renderer/shared/components/NewWallet/Import/Import/index.js
+++ src/renderer/shared/components/NewWallet/Import/Import/index.js
@@ -4,7 +4,7 @@ import { withData } from 'spunky';
 import withInitialCall from 'shared/hocs/withInitialCall';
 import accountActions from 'auth/actions/v5AccountsActions';
 
-import Import from './Impo./Import
+import Import from './Import';
 
 const mapAccountActionsDataToProps = (accounts) => ({
   accounts: accounts && accounts.profiles
